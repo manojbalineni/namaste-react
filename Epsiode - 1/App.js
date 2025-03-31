1a1998b8bc36e8d92a669b8d142f09ca3f8af581
Manoj <manoj.balineni04@gmail.com>
2025-03-31 08:08:15 +0530

    Added Eposide-3 Content

--- Epsiode - 1/App.js
+++ Epsiode - 1/App.js
@@ -1,11 +1,37 @@
 //Step -1 : Create an element.
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading",
-  },
-  "Hello World from React!"
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading",
+//   },
+//   "Hello World from React!"
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = (
+  <h1 className="head" tabIndex={1}>
+    Hello World from JSX
+  </h1>
 );
+
+const name = "Manoj Babu Balineni";
+
+const FooterComponent = () => (
+  <div className="footer">
+    {HeadingComponent()}
+    <h1>This is a functional Component without return </h1>
+  </div>
+);
+
+const HeadingComponent = () => {
+  return (
+    <h1 className="Heading-1">
+      Hello from the functional Componet with Return
+    </h1>
+  );
+};
 
 console.log(heading); // Object
 
@@ -13,4 +39,4 @@ console.log(heading); // Object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Step -3 : Render the created element to the root
-root.render(heading);
+root.render(<FooterComponent />);
